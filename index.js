18b0bf7b2ddadac9a2775d2593e1e7f72e0b92e9
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2021-12-12 00:48:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+
+// Crear el servidor/aplicacion de express
+const app = express();
+
+// Rutas
+app.use( '/api/auth', require('./routes/auth'));
+
+
+
+
+
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+});
