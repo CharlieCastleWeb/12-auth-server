8bc53efdbb8c83d22dd3b448e6597703cfc9471d
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2021-12-13 17:10:04 +0100

    Desarrollo auth controllers y middlewares

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 
 // Crear el servidor/aplicacion de express
 const app = express();
+
+// Directorio público
+app.use( express.static('public') );
+
+// CORS
+app.use( cors() );
+
+// Lectura y parseo del body
+app.use( express.json() );
 
 // Rutas
 app.use( '/api/auth', require('./routes/auth'));
@@ -11,6 +22,6 @@ app.use( '/api/auth', require('./routes/auth'));
 
 
 
-app.listen( 4000, () => {
-    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,28 @@
 
 const { Router } = require('express');
+const { check } = require('express-validator');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 // Crear un nuevo usuario
-router.post( '/new', (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario /new'
-    });
-});
+router.post( '/new', [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña es obligatoria').isLength({min: 6}),
+    validarCampos
+], crearUsuario );
 
 // Login de usuario
-router.post( '/', (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Login de usuario /'
-    });
-});
+router.post( '/', [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'La contraseña es obligatoria').isLength({min: 6}),
+    validarCampos
+], loginUsuario );
 
 // Validar y revalidar token
-router.get( '/renew', (req, res) => {
-    return res.json({
-        ok: true,
-        msg: 'Renew'
-    });
-});
+router.get( '/renew', revalidarToken);
 
 
 module.exports = router;
