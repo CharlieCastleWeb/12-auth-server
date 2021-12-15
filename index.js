022fa3ebe0164382f190f2a05d4dbdf68ce36447
CharlieCastleWeb <castillomartinezcarlos@gmail.com>
2021-12-15 11:22:38 +0100

    Listo para producción

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
+const res = require('express/lib/response');
 require('dotenv').config();
 
 // Crear el servidor/aplicacion de express
@@ -22,9 +24,10 @@ app.use( express.json() );
 // Rutas
 app.use( '/api/auth', require('./routes/auth'));
 
-
-
-
+// Manejar demás rutas
+app.get( '*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ));
+});
 
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
